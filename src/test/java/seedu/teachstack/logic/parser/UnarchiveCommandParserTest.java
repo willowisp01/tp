86ec8b383398e38e24fb0117d5871d2748a31cdf
Wongqingyee <qingyeewong@gmail.com>
2024-03-30 14:21:30 +0800

    Add UnarchiveCommandParserTest

--- src/test/java/seedu/teachstack/logic/parser/UnarchiveCommandParserTest.java
+++ src/test/java/seedu/teachstack/logic/parser/UnarchiveCommandParserTest.java
@@ -7,26 +7,27 @@ import static seedu.teachstack.testutil.TypicalStudentIds.ID_FIRST_PERSON;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.teachstack.logic.commands.ArchiveCommand;
+import seedu.teachstack.logic.commands.UnarchiveCommand;
 
 /**
  * As we are only doing white-box testing, our test cases do not cover path variations
- * outside of the ArchiveCommand code. For example, inputs "A0123456A" and "A0123456A abc" take the
- * same path through the ArchiveCommand, and therefore we test only one of them.
+ * outside of the UnarchiveCommand code. For example, inputs "A0123456A" and "A0123456A abc" take the
+ * same path through the UnarchiveCommand, and therefore we test only one of them.
  * The path variation for those two cases occur inside the ParserUtil, and
  * therefore should be covered by the ParserUtilTest.
  */
-public class ArchiveCommandParserTest {
+public class UnarchiveCommandParserTest {
 
-    private ArchiveCommandParser parser = new ArchiveCommandParser();
+    private UnarchiveCommandParser parser = new UnarchiveCommandParser();
 
     @Test
-    public void parse_validArgs_returnsArchiveCommand() {
-        assertParseSuccess(parser, "A0128956X", new ArchiveCommand(ID_FIRST_PERSON));
+    public void parse_validArgs_returnsUnarchiveCommand() {
+        assertParseSuccess(parser, "A0128956X", new UnarchiveCommand(ID_FIRST_PERSON));
     }
 
     @Test
     public void parse_invalidArgs_throwsParseException() {
-        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT, ArchiveCommand.MESSAGE_USAGE));
+        assertParseFailure(parser, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, UnarchiveCommand.MESSAGE_USAGE));
     }
 }
