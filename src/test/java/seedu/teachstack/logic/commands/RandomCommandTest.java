31e7172ec9f1f25a05e1e69d5171a5a13b616778
JiaXinEu <tis8cjiaxineu@gmail.com>
2024-03-30 20:27:47 +0800

    Add more tests

--- src/test/java/seedu/teachstack/logic/commands/RandomCommandTest.java
+++ src/test/java/seedu/teachstack/logic/commands/RandomCommandTest.java
@@ -3,20 +3,24 @@ package seedu.teachstack.logic.commands;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static seedu.teachstack.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.teachstack.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.teachstack.testutil.Assert.assertThrows;
 import static seedu.teachstack.testutil.TypicalPersons.ALICE;
+import static seedu.teachstack.testutil.TypicalPersons.FIONA;
 import static seedu.teachstack.testutil.TypicalPersons.GEORGE;
 import static seedu.teachstack.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import seedu.teachstack.logic.commands.exceptions.CommandException;
 import seedu.teachstack.model.AddressBook;
 import seedu.teachstack.model.Model;
 import seedu.teachstack.model.ModelManager;
 import seedu.teachstack.model.UserPrefs;
+import seedu.teachstack.model.group.Group;
 import seedu.teachstack.model.person.Person;
 import seedu.teachstack.testutil.PersonBuilder;
 
@@ -40,15 +44,49 @@ public class RandomCommandTest {
     @Test
     public void execute_validNameAndNumber_success() {
         RandomCommand randomCommand = new RandomCommand(GROUP_NAME_A, 1);
-        Person editedPerson = new PersonBuilder(GEORGE).withGroups(GROUP_NAME_A + " 1").build();
-        Person editedPerson2 = new PersonBuilder(ALICE).withGrade("C").withGroups(GROUP_NAME_A + " 1").build();
+        Person editedPerson2 = new PersonBuilder(ALICE).withGrade("C").withGroups().build();
         model.setPerson(model.getPerson(ALICE.getStudentId()), editedPerson2);
 
+        Person editedPerson = new PersonBuilder(GEORGE).withGroups(GROUP_NAME_A + " 1").build();
+        Person editedPerson2Copy = new PersonBuilder(editedPerson2).withGroups(GROUP_NAME_A + " 1").build();
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+        expectedModel.setPerson(model.getPerson(ALICE.getStudentId()), editedPerson2Copy);
         expectedModel.setPerson(model.getPerson(GEORGE.getStudentId()), editedPerson);
-        expectedModel.setPerson(model.getPerson(ALICE.getStudentId()), editedPerson2);
         assertCommandSuccess(randomCommand, model,
                 String.format(RandomCommand.MESSAGE_RANDOM_GROUP_SUCCESS, 1, GROUP_NAME_A), expectedModel);
+    }
+
+    @Test
+    public void execute_validNameAndNumberUnequalGroup_success() {
+        RandomCommand randomCommand = new RandomCommand(GROUP_NAME_A, 2);
+        Person editedPerson2 = new PersonBuilder(ALICE).withGrade("C").withGroups().build();
+        model.setPerson(model.getPerson(ALICE.getStudentId()), editedPerson2);
+        Person editedPerson3 = new PersonBuilder(FIONA).withGrade("D").withGroups().build();
+        model.setPerson(model.getPerson(FIONA.getStudentId()), editedPerson3);
+
+        try {
+            randomCommand.execute(model);
+            if (model.getPerson(ALICE.getStudentId()).getGroups().contains(new Group(GROUP_NAME_A + " 1"))) {
+                if (model.getPerson(GEORGE.getStudentId()).getGroups().contains(new Group(GROUP_NAME_A + " 1"))) {
+                    assertTrue(model.getPerson(FIONA.getStudentId())
+                            .getGroups().contains(new Group(GROUP_NAME_A + " 2")));
+
+                } else if (model.getPerson(GEORGE.getStudentId())
+                        .getGroups().contains(new Group(GROUP_NAME_A + " 2"))) {
+                    assertTrue(model.getPerson(FIONA.getStudentId())
+                            .getGroups().contains(new Group(GROUP_NAME_A + " 1")));
+                }
+            } else if (model.getPerson(ALICE.getStudentId()).getGroups().contains(new Group(GROUP_NAME_A + " 2"))) {
+                assertTrue((model.getPerson(FIONA.getStudentId())
+                        .getGroups().contains(new Group(GROUP_NAME_A + " 1")))
+                        && (model.getPerson(GEORGE.getStudentId())
+                        .getGroups().contains(new Group(GROUP_NAME_A + " 1"))));
+            }
+        } catch (CommandException e) {
+            fail();
+        }
+
+
     }
 
     @Test
